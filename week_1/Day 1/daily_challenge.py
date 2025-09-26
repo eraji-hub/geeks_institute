b9321d268db971ea7edf2e42bff6e03094186a95
Ayoub eraji <78859537+eraji-hub@users.noreply.github.com>
2025-09-26 10:34:57 +0100

    Update daily_challenge.py

--- week_1/Day 1/daily_challenge.py
+++ week_1/Day 1/daily_challenge.py
@@ -1,3 +1,5 @@
+#Challenge 1
+
 number = int(input("Enter the number: "))
 length = int(input("Enter the length of numbers: "))
 
@@ -6,3 +8,16 @@ multiples = []
 for i in range(1, length+1):
     multiples.append(number * i)
 print(multiples)
+
+#hallenge 2
+
+def remove_duplicates(word):
+    result = ""
+    for char in word:
+        if not result or char != result[-1]:
+            result += char
+    return result
+
+# Ask user for input
+user_word = input("Enter a word: ")
+print("Result:", remove_duplicates(user_word))
